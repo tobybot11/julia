a727a5b3f857cbf7db3f46a694bf65ea1a4b1958
Tobias Ford <toby.ford@pobox.com>
2014-02-03 22:32:15 -0500

    adding in return function

--- quadeq/derivative.jl
+++ quadeq/derivative.jl
@@ -1,5 +1,16 @@
 # in derivative.jl
 
 function derivative(f)
-	return f
+	# Look : you're reutrn a function of x
+
+
+	return function(x)  # yet another way to define functions in Julia
+		# pick a small value for h
+		# evaluate f at x+h
+		# evaluate f at x
+		# divide the difference by h
+	end
 end
+
+# derivative((x) -> x^2 + 2x +3 ) returns a function that approximates (x) -> 2x+2	
+
