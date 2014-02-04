e8520045281fb4631d15df7b5e992596d8022512
Tobias Ford <toby.ford@pobox.com>
2014-02-03 22:33:43 -0500

    adding in the code for the return function

--- quadeq/derivative.jl
+++ quadeq/derivative.jl
@@ -2,13 +2,18 @@
 
 function derivative(f)
 	# Look : you're reutrn a function of x
-
-
 	return function(x)  # yet another way to define functions in Julia
 		# pick a small value for h
+		h = 0.00001
+
 		# evaluate f at x+h
+		f1 = f(x + h)
+
 		# evaluate f at x
+		f0 = f(x)
+
 		# divide the difference by h
+		return (f1 - f0) / h 
 	end
 end
 
